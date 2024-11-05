93cdbab61e36066112faaabc3341fdda5480d1cb
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com>
2024-11-05 12:32:37 -0500

    Solution exercise 4

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-firstVolt = int(input("Enter the first volt: "))
-secondVolt = int(input("Enter the second volt: "))
-thirdVolt = int(input("Enter the third volt: "))
+meters = int(input("Enter a length in meters: "))
 
-averageVolt = (firstVolt + secondVolt + thirdVolt) / 3 
+km = meters / 1000
 
-if averageVolt < 115:
-    print("VOLTAJE CORRECTO")
-elif averageVolt > 115 and averageVolt < 220:
-    print("ALTO VOLTAJE")
-elif averageVolt > 220:
-    print("PELIGRO")
+print(f"{meters}m in kilometers is {km} km")
